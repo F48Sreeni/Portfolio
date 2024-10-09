b93de63796b6fa2bebca317ca5aea05bb83b33ef
Sreenitha Garipelli <154340405+F48Sreeni@users.noreply.github.com>
2024-10-09 19:00:34 +0000

    Updates

---
+++ precache-manifest.7bc00b5e1f717556af5b33fd8905b2ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3634650278ecfb604e4ffe2ac03a30e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "194fb396d42bdd18b732",
+    "url": "/static/css/main.3f8189e9.chunk.css"
+  },
+  {
+    "revision": "9f1ce303ce5648505dbc",
+    "url": "/static/js/2.d07c4526.chunk.js"
+  },
+  {
+    "revision": "b95080709ac12d0967db165d90e6c04a",
+    "url": "/static/js/2.d07c4526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "194fb396d42bdd18b732",
+    "url": "/static/js/main.23e5ba60.chunk.js"
+  },
+  {
+    "revision": "7c5e4ace7e9c5a53bd8e",
+    "url": "/static/js/runtime-main.06e93911.js"
+  },
+  {
+    "revision": "0d08c84198da04aecd125b39d878e66d",
+    "url": "/static/media/blood-donor.0d08c841.jpg"
+  },
+  {
+    "revision": "3605793adce6d949362d0e45e01e2103",
+    "url": "/static/media/flightnex.3605793a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.f2622e94fb211a86c92df113500dad06.js"
+  "/precache-manifest.7bc00b5e1f717556af5b33fd8905b2ab.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
