8593cb5a8b154dd5e9bf8fac9b20be0bb9feee5d
Sreenitha Garipelli <154340405+F48Sreeni@users.noreply.github.com>
2024-10-09 17:11:45 +0000

    Updates

---
+++ precache-manifest.56922873804e0b288b2a78d591641a06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f148024387153ec8bab645f26f157e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "1022063e647687d92a35",
+    "url": "/Portfolio/static/css/main.3f8189e9.chunk.css"
+  },
+  {
+    "revision": "74f9e72d13f3a5b4cd06",
+    "url": "/Portfolio/static/js/2.5a6729a2.chunk.js"
+  },
+  {
+    "revision": "b95080709ac12d0967db165d90e6c04a",
+    "url": "/Portfolio/static/js/2.5a6729a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1022063e647687d92a35",
+    "url": "/Portfolio/static/js/main.ef345e37.chunk.js"
+  },
+  {
+    "revision": "da63585c5e2f12679659",
+    "url": "/Portfolio/static/js/runtime-main.90efdbf3.js"
+  }
+]);
