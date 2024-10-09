8e5cd5cb0a6fa2153374263a8ff8fcc10a6c887b
Sreenitha Garipelli <154340405+F48Sreeni@users.noreply.github.com>
2024-10-09 18:02:16 +0000

    Changes

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -11,7 +11,8 @@ const Background = styled.div`
 `;
 
 const ProfileImage = styled.img`
-  width: 150px;
+  width: 250px;
+  height: 150px;
   border-radius: 1%;
   box-shadow: 0 4px 15px rgba(0, 0, 0, 0.3);
   transition: transform 0.3s;
